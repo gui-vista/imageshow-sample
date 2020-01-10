6b76a8f72176939e5680acf7b8f9bba2c33510f0
Nick Apperley <napperley@gmail.com>
2020-01-11 11:53:25 +1300

    Printed image names.

--- src/linuxX64Main/kotlin/main.kt
+++ src/linuxX64Main/kotlin/main.kt
@@ -2,6 +2,7 @@
 
 package org.example.image_show
 
+import gio2.G_FILE_ATTRIBUTE_STANDARD_NAME
 import gio2.G_FILE_QUERY_INFO_NONE
 import gio2.G_FILE_TYPE_DIRECTORY
 import glib2.gpointer
@@ -15,19 +16,46 @@ import org.guiVista.core.fetchEmptyDataPointer
 import org.guiVista.io.File
 import platform.posix.getenv
 
+private const val PNG_EXT = ".png"
+private const val TIF_EXT = ".tif"
+private const val JPEG_EXT = ".jpeg"
+private val picturesDir = File.fromPath("${getenv("HOME")?.toKString()}/Pictures")
+val picturesDirPath = picturesDir.path
+val images by lazy { fetchImageFiles(picturesDir) }
 private lateinit var mainWin: MainWindow
 private var activateHandlerId = 0uL
 
 fun main() {
-    val picturesDir = File.fromPath("${getenv("HOME")?.toKString()}/Pictures")
-    val fileType = picturesDir.queryFileType(G_FILE_QUERY_INFO_NONE)
-    if (fileType == G_FILE_TYPE_DIRECTORY) println("${picturesDir.baseName} is a directory.")
+    printImageNames()
     Application(id = "org.example.image-show").use {
         mainWin = MainWindow(this)
         activateHandlerId = connectActivateSignal(staticCFunction(::activateApplication), fetchEmptyDataPointer())
         connectShutdownSignal(staticCFunction(::shutdownApplication), fetchEmptyDataPointer())
         println("Application Status: ${run()}")
     }
+}
+
+private fun printImageNames() {
+    if (images.isNotEmpty()) {
+        println("-- Images (from $picturesDirPath) --")
+        images.forEach { println("* $it") }
+    }
+}
+
+private fun fetchImageFiles(dir: File): Array<String> {
+    val tmp = mutableListOf<String>()
+    val fileType = dir.queryFileType(G_FILE_QUERY_INFO_NONE)
+    if (fileType == G_FILE_TYPE_DIRECTORY) {
+        val enumerator = dir.enumerateChildren(G_FILE_ATTRIBUTE_STANDARD_NAME, 0u)
+        var fileInfo = enumerator?.nextFile()
+        while (fileInfo != null) {
+            tmp += fileInfo.name
+            fileInfo.close()
+            fileInfo = enumerator?.nextFile()
+        }
+        enumerator?.close()
+    }
+    return tmp.filter { it.endsWith(PNG_EXT) || it.endsWith(TIF_EXT) || it.endsWith(JPEG_EXT) }.toTypedArray()
 }
 
 @Suppress("UNUSED_PARAMETER")
