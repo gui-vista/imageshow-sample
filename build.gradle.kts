7d76db4cdc1b476bbd1b1eda7214af462c1fbbd3
Nick Apperley <napperley@gmail.com>
2020-05-01 15:22:22 +1200

    Added def files, and used remote GUI Vista Maven repositories.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,36 @@ group = "org.example"
 version = "1.0-SNAPSHOT"
 
 plugins {
-    kotlin("multiplatform") version "1.3.61"
+    kotlin("multiplatform") version "1.3.72"
 }
 
 repositories {
-    mavenLocal()
+    jcenter()
+    mavenCentral()
+    maven {
+        val guiVistaCore = "16245519"
+        url = uri("https://gitlab.com/api/v4/projects/$guiVistaCore/packages/maven")
+    }
+    maven {
+        val guiVistaIo = "16243425"
+        url = uri("https://gitlab.com/api/v4/projects/$guiVistaIo/packages/maven")
+    }
+    maven {
+        val guiVistaGui = "15889948"
+        url = uri("https://gitlab.com/api/v4/projects/$guiVistaGui/packages/maven")
+    }
 }
 
 kotlin {
     linuxX64 {
         compilations.getByName("main") {
             dependencies {
-                val guiVistaVer = "0.1-SNAPSHOT"
-                implementation("org.guivista:guivista-gui-linuxx64:$guiVistaVer")
-                implementation("org.guivista:guivista-io-linuxx64:$guiVistaVer")
+                val guiVistaVer = "0.1.1"
+                cinterops.create("glib2")
+                cinterops.create("gio2")
+                cinterops.create("gtk3")
+                implementation("org.guivista:guivista-gui:$guiVistaVer")
+                implementation("org.guivista:guivista-io:$guiVistaVer")
             }
         }
         binaries {
@@ -25,7 +41,8 @@ kotlin {
         }
 
         sourceSets {
-            @Suppress("UNUSED_VARIABLE") val linuxX64Main by getting {
+            @Suppress("UNUSED_VARIABLE")
+            val linuxX64Main by getting {
                 languageSettings.useExperimentalAnnotation("kotlin.ExperimentalUnsignedTypes")
             }
         }
